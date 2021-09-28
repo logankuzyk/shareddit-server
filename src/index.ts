e3e43a10ad13350b2f239f6122c8f5ee4101ff18
Logan Kuzyk <logan@kuzyk.net>
2021-09-28 14:33:22 -0700

    Bind to 0.0.0.0

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,9 @@ import * as corsProxy from './corsProxy';
 const dotenv = require('dotenv').config();
 
 // Start the server
-const port = Number(4000);
-http.createServer(app).listen(port);
+const port = 4000;
+http.createServer(app).listen(port, '0.0.0.0', () => {
+  console.log(`shareddit server started on port ${port}`);
+});
 
 corsProxy.start();
